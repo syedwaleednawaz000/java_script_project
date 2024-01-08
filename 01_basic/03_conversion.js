7bcb39bba1fe9bdf5b9c5315048768e0b19790bd
syedwaleednawaz000 <syedwaleednawaz333@gmail.com>
2024-01-08 10:16:19 +0500

    conversion

---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,15 @@
+let age = false
+
+
+console.log(typeof age)
+let newAge = Number(age)
+console.log(typeof newAge)
+console.log(newAge)
+////////////////
+let logIn = 0
+let checkLogIn = Boolean(logIn)
+console.log(checkLogIn)
+//////////////////
+let someNumber = 3343
+let newString = String(someNumber)
+console.log(newString);
--- 01_basic/o2_datatypes.js
+++ 01_basic/o2_datatypes.js
@@ -1,6 +1,6 @@
 let name = "waleed"
-let age = 24
+let  age = 24
 let state ;
 let contact = 34567890
 let email =  null
-console.log(typeof name)
+console.log(typeof contact)
