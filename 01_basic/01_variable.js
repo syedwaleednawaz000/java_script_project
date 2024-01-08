0befaa526577a27fbdb3b00bec4f5a3226394de4
syedwaleednawaz000 <syedwaleednawaz333@gmail.com>
2024-01-08 09:52:31 +0500

    new changing

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,8 @@
+const accountId = 123
+let accountEmil = "waleed@gmail.com"
+// preffer only  the above method beacause due to some issues like scope
+// and functional 
+var accountPasssword = "12345"
+accountCity = "islamabad"
+let accountState ;
+console.table([accountId,accountEmil,accountPasssword,accountCity,accountState])
---
+++ 01_basic/o2_datatypes.js
@@ -0,0 +1,6 @@
+let name = "waleed"
+let age = 24
+let state ;
+let contact = 34567890
+let email =  null
+console.log(typeof name)
